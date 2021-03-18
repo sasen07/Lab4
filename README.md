5a0c6ad5e5b014111230a83b1062b5362bcdce6d
Sasenshrestha <sasensht42@gmail.com>
2021-03-18 10:26:52 +0545

    Coding CHallenge done

---
+++ README.md
@@ -0,0 +1,5 @@
+# Lab4
+## Coding_challenge_screenshot
+![](./cc1.png)
+![](./cc2.png)
+
---
+++ app/src/main/java/com/example/twoactivities/Second.java
@@ -0,0 +1,21 @@
+package com.example.twoactivities;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Second extends AppCompatActivity {
+
+    private TextView displayText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        displayText = findViewById(R.id.display_text);
+        Intent intent = getIntent();
+        String display = intent.getStringExtra(MainActivity.EXTRA_TEXT);
+        displayText.setText(display);
+    }
+}
